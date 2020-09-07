1cfdd1d11994d0a8ace47e584ca7fde4f7e6344d
Ashraf Kamarudin <ashrafkamarudin1995@gmail.com>
2020-09-08 02:26:38 +0800

    added progress

--- classes/scrapper.py
+++ classes/scrapper.py
@@ -1,20 +1,25 @@
 
 import re, requests
+from classes.progresss import Progress
 
 class Scrapper:
     scrappedItems = []
 
     def __init__(self, urls):
         self.urls = urls
+        self.progress = Progress(len(urls))
         self.scrap()
 
     def scrap(self):
         failedUrls = []
         for url in self.urls:
+            self.progress.cont()
             try:
                 request=requests.get(url)
+                self.progress.print(" Scrapped :min url out of :max")
             except Exception as e:
                 failedUrls.append(url)
+                self.progress.print(" Skipped " + url)
 
             self.scrappedItems.append(ScrappedPageStruct(
                 url = url,
