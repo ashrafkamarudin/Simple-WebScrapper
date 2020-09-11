708e56cc3bcfc9b039b9cf115c8cd1704fd1174b
Ashraf Kamarudin <ashrafk@runcloud.io>
2020-09-11 10:00:48 +0800

    rename columnClosure to functions and seperate config from main

---
+++ config.py
@@ -0,0 +1,66 @@
+from functions import *
+
+# Default Configurations #
+config = {
+    # File to be written to
+    'write_to': 'test.csv',
+
+    # File to read from and which column
+    'read_from': {
+        'name': 'urls.csv',
+        'column': 'A'
+    },
+
+    # Number of threads or worker. 
+    # The higher it is, The faster the script will run
+    'number_of_thread': 300,
+
+    'column_to_append': {
+
+        # First Column
+        'URL'                       : firstColumn(),
+
+        # #1 Count on Script
+        'escape()'                  : calc("escape("),
+        'unescape()'                : calc("unescape("),
+        'eval()'                    : calc("eval("),
+        'search()'                  : calc("search("),
+        'charAt()'                  : calc("charAt("),
+        'charCodeAt()'              : calc("charCodeAt("),
+        'Concat()'                  : calc("Concat("),
+        'indexOf()'                 : calc("indexOf("),
+        'substring()'               : calc("substring("),
+        'replace()'                 : calc("replace("),
+        'Split()'                   : calc("Split("),
+        'toString()'                : calc("toString("),
+        'document.write()'          : calc("document.write("),
+        'Window.location()'         : calc("Window.location("),
+        'Document.cookie'           : calc("Document.cookie"),
+        'fromCharCode()'            : calc("fromCharCode("),
+        'exec()'                    : calc("exec("),
+        'setInterval()'             : calc("setInterval("),
+        'setTimeout()'              : calc("setTimeout("),
+        '<iframe>'                  : calc("<iframe"),
+        '<a>'                       : calc("<a"),
+        'onload()'                  : calc("onload("),
+        'onerror'                   : calc("onerror"),
+        'onunload'                  : calc("onunload"),
+        'onmouseover'               : calc("onmouseover"),
+        'onbeforeunload'            : calc("onbeforeunload"),
+        'addEventListener()'        : calc("addEventListener("),
+        'attachEvent()'             : calc("attachEvent("),
+        'dispatchEvent()'           : calc("dispatchEvent("),
+
+        # #2 URL Length
+        'URL Length'                : urlLenght(),
+
+        # #3 IP Address
+        'IP Address'                : ipAddress(),
+
+        # #4 Special Characters
+        'Special Character Count'   : countSpecialCharacterInUrl(["?", "-", "_", "=", "%"]),
+
+        # #5 Commenting Style
+        'Commenting Style'          : commentStyle()
+    }
+}
--- main.py
+++ main.py
@@ -1,75 +1,14 @@
 import csv, sys, time
 from classes.scrapper import Scrapper, ScrappedPageStruct
 from classes.file import File
-from columnClosure import *
 from classes import log
 
-# Default Configurations #
-config = {
-    # File to be written to
-    'write_to': 'test.csv',
-
-    # File to read from and which column
-    'read_from': {
-        'name': 'urls.csv',
-        'column': 'A'
-    },
-
-    # Number of threads or worker. 
-    # The higher it is, The faster the script will run
-    'number_of_thread': 300,
-
-    'column_to_append': {
-
-        # First Column
-        'URL'                       : firstColumn(),
-
-        # #1 Count on Script
-        'escape()'                  : calc("escape("),
-        'unescape()'                : calc("unescape("),
-        'eval()'                    : calc("eval("),
-        'search()'                  : calc("search("),
-        'charAt()'                  : calc("charAt("),
-        'charCodeAt()'              : calc("charCodeAt("),
-        'Concat()'                  : calc("Concat("),
-        'indexOf()'                 : calc("indexOf("),
-        'substring()'               : calc("substring("),
-        'replace()'                 : calc("replace("),
-        'Split()'                   : calc("Split("),
-        'toString()'                : calc("toString("),
-        'document.write()'          : calc("document.write("),
-        'Window.location()'         : calc("Window.location("),
-        'Document.cookie'           : calc("Document.cookie"),
-        'fromCharCode()'            : calc("fromCharCode("),
-        'exec()'                    : calc("exec("),
-        'setInterval()'             : calc("setInterval("),
-        'setTimeout()'              : calc("setTimeout("),
-        '<iframe>'                  : calc("<iframe"),
-        '<a>'                       : calc("<a"),
-        'onload()'                  : calc("onload("),
-        'onerror'                   : calc("onerror"),
-        'onunload'                  : calc("onunload"),
-        'onmouseover'               : calc("onmouseover"),
-        'onbeforeunload'            : calc("onbeforeunload"),
-        'addEventListener()'        : calc("addEventListener("),
-        'attachEvent()'             : calc("attachEvent("),
-        'dispatchEvent()'           : calc("dispatchEvent("),
-
-        # #2 URL Length
-        'URL Length'                : urlLenght(),
-
-        # #3 IP Address
-        'IP Address'                : ipAddress(),
-
-        # #4 Special Characters
-        'Special Character Count'   : countSpecialCharacterInUrl(["?", "-", "_", "=", "%"]),
-
-        # #5 Commenting Style
-        'Commenting Style'          : commentStyle()
-    }
-}
+import config
 
 # Begin Operation
+# get config
+config = config.config
+
 # Init file
 file = File(name = config['write_to'])
 
@@ -95,8 +34,8 @@ print("\nWriting to file ...")
 # file.write()
 print("\nDumping Log to file ...")
 
-logFile = open('logs/scrapper.log', 'a+')
-logFile.writelines(str(log.dump()))
-logFile.close()
+with open('logs/scrapper.log', 'a+') as f:
+    f.writelines(str(log.dump()))
+    f.close()
 
 print("Operation finished")
